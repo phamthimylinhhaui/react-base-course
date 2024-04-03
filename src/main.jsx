33c3bc811957d3dacb4d34d64ed6f32243440aaf
hapo-linhptm <linh.ptm@haposoft.com>
2024-04-03 21:49:55 +0700

    lession-40: Æworking with layout routers

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import NewPost from "./components/NewPost.jsx";
+import RootLayout from "./routers/RootLayout.jsx";
 
 const router = createBrowserRouter([
-    { path: '/', element: <App/> },
-    { path: '/post/create', element: <NewPost /> }
+    {
+        path: '/', element: <RootLayout/>,
+        children: [
+            {path: '/', element: <App/>},
+            {path: '/post/create', element: <NewPost/>}
+        ]
+    },
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
